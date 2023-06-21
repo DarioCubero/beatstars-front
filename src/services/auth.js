fa0cd5871dab1275d4a99d38db579a9cc2792af7
Dario Cubero <cubero@dxc.com>
2023-06-21 06:23:28 +0200

    fix env file

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,24 +1,17 @@
 // Librería JavaScript que puede ejecutarse en el navegador y que nos permite hacer sencillas las operaciones como cliente HTTP
 import axios from "axios"
 
-// const API_URL = "https://beatstars.azurewebsites.net/api/auth/";
-// const API_URL = "https://localhost:7209/api/auth/";
-// const API_URL = "http://+:8001/api/auth/";
-
 let API_URL = "";
-if (process.env.VUE_APP_MODE == "dev") {
+if (process.env.VUE_APP_MODE.toUpperCase() == "DEV") {
 	API_URL = process.env.VUE_APP_API_URL_DEV;
 }
-
-if (process.env.VUE_APP_MODE == "compose") {
+if (process.env.VUE_APP_MODE.toUpperCase() == "COMPOSE") {
 	API_URL = process.env.VUE_APP_API_URL_COMPOSE;
 }
-
-if (process.env.VUE_APP_MODE == "azure") {
+if (process.env.VUE_APP_MODE.toUpperCase() == "AZURE") {
 	API_URL = process.env.VUE_APP_API_URL_AZURE;
 }
 
-// process.env.API_URL
 const USER_LOGIN = API_URL + "/api/auth/login";
 const REGISTER_URL = API_URL + "/api/auth/register";
 
